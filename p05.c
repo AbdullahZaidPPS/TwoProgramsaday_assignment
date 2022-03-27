82c3b2047c7e2bd3467f70dc42f4947502807546
Abdullah M Zaid <95867207+AbdullahZaidPPS@users.noreply.github.com>
2022-03-27 15:15:48 +0000

    5th program

--- p05.c
+++ p05.c
@@ -1,1 +1,32 @@
-
+#include<stdio.h>
+int input()
+{
+  int c;
+  printf("Enter the number\n");
+  scanf("%d",&c);
+  return c;
+}
+int gcd(int a,int b)
+{
+  int n;
+  while(b!=0)
+    {
+      n=b;
+      b=a%b;
+      a=n;
+    }
+   return a;
+}
+void output(int a,int b,int gcd)
+{
+  printf("The gcd of %d and %d is %d",a,b,gcd);
+}
+int main()
+{
+  int x,y,z;
+  x=input();
+  y=input();
+  z=gcd(x,y);
+  output(x,y,z);
+  return 0;
+}
