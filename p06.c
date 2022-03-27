c1f8a38f87c2b777602e26253349b4a0aa748e3e
Abdullah M Zaid <95867207+AbdullahZaidPPS@users.noreply.github.com>
2022-03-27 15:31:50 +0000

    6th program

--- p06.c
+++ p06.c
@@ -1,1 +1,31 @@
-
+#include<stdio.h>
+void input_string(char *a)
+{
+  printf("Enter the string\n");
+  scanf("%s",a);
+}
+int str_reverse(char *a)
+{
+  int b;
+  for(int i=0;a[i]!='\0';i++)
+    {
+      b=i;
+    }
+  return b;
+}
+void output(char *a,int reversea)
+{
+  for(int j=reversea;j>=0;j--)
+    {
+      printf("%c",a[j]);
+    }
+}
+int main()
+{
+  char b[50];
+  int y;
+  input_string(b);
+  y=str_reverse(b);
+  output(b,y);
+  return 0;
+}
